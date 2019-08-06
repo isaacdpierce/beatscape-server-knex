027b02606be9dfa03f8e1ae511bdc3d132c233f9
Isaac Pierce <contact@isaacdpierce.com>
2019-08-05 18:31:07 -0700

    Creates axios function to get audio arraybuffer from AWS. Still too slow so commented out in soundscapes-router.js

--- src/routers/soundscapes-router.js
+++ src/routers/soundscapes-router.js
@@ -28,3 +28,28 @@ soundscapesRouter.route('/:soundscape_id').get((req, res, next) => {
 });
 
 module.exports = soundscapesRouter;
+
+// ! Code for fetching aws data on server - works but currently too slow - need to do more research
+// ! If using - need to require axios
+// soundscapesRouter.route('/:soundscape_id').get((req, res, next) => {
+//   const knexInstance = req.app.get('db');
+//   SoundscapesService.getById(knexInstance, req.params.soundscape_id)
+//     .then(async dbResponse => {
+//       const urls = dbResponse.stems.map(stem => stem.sources[0]);
+//       const promises = urls.map(async url => {
+//         const response = await axios({
+//           responseType: 'arraybuffer',
+//           url: url,
+//           method: 'get',
+//           headers: {
+//             'Content-Type': 'audio/mp3',
+//           },
+//         })
+//         console.log(response.data.data);
+//         return response.data
+//       })
+//       const results = await Promise.all(promises)
+//       res.json(results)
+//     })
+//     .catch(next);
+// });
